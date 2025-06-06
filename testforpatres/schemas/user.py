de3227ef9033121c2430033d5ec8a9a290fb9069
Valerii <spaceocean@outlook.com>
2025-06-06 18:48:21 +0500

    added documentation in readme

--- testforpatres/schemas/user.py
+++ testforpatres/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from typing import Optional
 
 class Token(BaseModel):
@@ -20,5 +20,4 @@ class UserResponse(BaseModel):
     id: int
     email: EmailStr
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
