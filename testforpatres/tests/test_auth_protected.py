ee85691b89ac6b9459d60206f3f45689181fce97
Valerii <spaceocean@outlook.com>
2025-06-06 19:29:11 +0500

    readme.md supplemented

--- testforpatres/tests/test_auth_protected.py
+++ testforpatres/tests/test_auth_protected.py
@@ -1,12 +1,14 @@
-from fastapi.testclient import TestClient
-from testforpatres.main import app
-from fastapi import status
 import uuid
+from fastapi.testclient import TestClient
+from fastapi import status
+from testforpatres.main import app  # замени путь при необходимости
 
 client = TestClient(app)
 
-def test_protected_endpoint_access():
+
+def test_access_control_for_endpoints():
     unique_email = f"user_{uuid.uuid4()}@example.com"
+
     response = client.post("/auth/register", json={
         "email": unique_email,
         "password": "string"
@@ -21,9 +23,12 @@ def test_protected_endpoint_access():
 
     token = response.json()["access_token"]
     headers = {"Authorization": f"Bearer {token}"}
-    protected_response = client.get("/books/", headers=headers)
-    assert protected_response.status_code == status.HTTP_200_OK
 
-    unprotected_response = client.get("/readers/")
-    assert unprotected_response.status_code == status.HTTP_403_FORBIDDEN
+    public_response = client.get("/books/")
+    assert public_response.status_code == status.HTTP_200_OK
 
+    unauthorized_response = client.get("/readers/")
+    assert unauthorized_response.status_code == status.HTTP_403_FORBIDDEN
+
+    authorized_response = client.get("/readers/", headers=headers)
+    assert authorized_response.status_code == status.HTTP_200_OK
